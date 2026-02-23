faf9c1696cdab5030d24e092ac55d4a353dd0029
hiteshwar3775 <hiteswar3775@gmail.com>
2026-02-23 08:30:39 +0530

    Initial commit for Step Java project

---
+++ UC2.java
@@ -0,0 +1,13 @@
+class UC2{
+    public static void main(String[] args) {
+        System.out.println(" ********  " + " " + "  *******  " + " " + "  *******  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "*********  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("  *******   " + " " + " *******  " + " " + "**         " + " " + "*******");
+
+    }
+}
